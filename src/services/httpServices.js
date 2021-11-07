2c83183e3f33e17c5650767f737af0a92b593a28
Tiilon <72743441+Tiilon@users.noreply.github.com>
2021-11-07 12:29:56 +0000

    Update httpServices.js

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:8000/api/"
+const baseURL = "https://sparow-clinic-admin.herokuapp.com"
 
 export const authAxios = axios.create({
     baseURL:baseURL,
